14951f0cc257bf6c1fe7be8f5e42bb0bd16ff4f8
Victor Villazon <victorzn23@gmail.com>
2014-10-13 18:08:26 -0500

    Configuración de base de datos a postgresql

--- eventus/eventus/settings/local.py
+++ eventus/eventus/settings/local.py
@@ -12,8 +12,12 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'eventusdb',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
